89994aedb92d6d45bacf1071ad4fa1cc01e205aa
pjooklas <pjooklas@users.noreply.github.com>
2022-04-06 18:02:54 +0300

    Preparation for data sending

--- public/js/pages/auth.js
+++ public/js/pages/auth.js
@@ -1,3 +1,5 @@
+import { IsValid } from '../components/is-valid/IsValid.js';
+
 /*
 1) susirasti forma ir jos VISUS laukus
 2) surinkti informacija is formos
@@ -17,18 +19,26 @@ const submitDOM = formDOM.querySelector('button');
 submitDOM.addEventListener('click', (e) => {
     e.preventDefault();
 
+    const errors = [];
     const formData = {};
     for (const inputDOM of allInputsDOM) {
-        const { id, value } = inputDOM;
+        const { id, value, dataset } = inputDOM;
+        const validationRule = IsValid[dataset.validation];
+        const [err, status] = validationRule(value);
+        if (err) {
+            errors.push(status);
+        }
         formData[id] = value;
     }
 
-    if (formData.pass === formData.repass) {
-        errorsDOM.innerText = '';
-    } else {
-        errorsDOM.innerText = 'Nesutampa slaptazodziai';
-        return;
+    if (formData.password !== formData.repass) {
+        errors.push('Nesutampa slaptazodziai');
+    }
+    errorsDOM.innerText = errors.join('\r\n');
+
+    if (errors.length === 0) {
+        delete formData.repass;
+        console.log('SIUNCIAME I SERVERI:', formData);
     }
 
-    console.log('SIUNCIAME I SERVERI:', formData);
 })
